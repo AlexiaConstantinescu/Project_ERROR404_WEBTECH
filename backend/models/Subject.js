e4c3d09d4ba1369c7a94017ec209ddd722118986
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-12-07 15:53:02 +0200

    Update Subject.js

--- backend/models/Subject.js
+++ backend/models/Subject.js
@@ -1,1 +1,47 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Subject = sequelize.define('Subject', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [1, 100]
+    }
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  color: {
+    type: DataTypes.STRING(7),
+    defaultValue: '#3B82F6',
+    validate: {
+      is: /^#[0-9A-F]{6}$/i
+    }
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  }
+}, {
+  tableName: 'subjects',
+  indexes: [
+    {
+      unique: true,
+      fields: ['name', 'userId']
+    }
+  ]
+});
+
+module.exports = Subject;
