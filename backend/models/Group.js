33bcdbb383fa3507ae02de332ac24c2a5f85b5ff
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-12-07 15:51:28 +0200

    Update Group.js

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -1,1 +1,38 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Group = sequelize.define('Group', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [1, 100]
+    }
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  ownerId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  },
+  isPrivate: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  tableName: 'groups'
+});
+
+module.exports = Group;
