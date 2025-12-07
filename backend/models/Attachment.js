c2f0b50b2425535db4ac9693949adfe1807ee22f
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-12-07 15:51:08 +0200

    Update Attachment.js

--- backend/models/Attachment.js
+++ backend/models/Attachment.js
@@ -1,1 +1,50 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Attachment = sequelize.define('Attachment', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  filename: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  originalName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mimetype: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  path: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  noteId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'notes',
+      key: 'id'
+    }
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  }
+}, {
+  tableName: 'attachments'
+});
+
+module.exports = Attachment;
