cf045314d876dcad73a09cb652c3c641648a1bec
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-12-07 15:55:30 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,72 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const { sequelize } = require('../config/database');
 
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+      isASEEmail(value) {
+        if (!value.endsWith('@stud.ase.ro')) {
+          throw new Error('Only @stud.ase.ro emails allowed');
+        }
+      }
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true
+  },
+  avatar: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  tableName: 'users',
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password') && user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    }
+  }
+});
+
+User.prototype.comparePassword = async function(candidatePassword) {
+  if (!this.password) return false;
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+User.prototype.toJSON = function() {
+  const values = { ...this.get() };
+  delete values.password;
+  return values;
+};
+
+module.exports = User;
