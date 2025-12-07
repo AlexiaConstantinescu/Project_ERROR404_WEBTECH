6bfd28476e1fe7c7cd87414d64033c0847018b19
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-12-07 15:55:02 +0200

    Update Note.js

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,1 +1,51 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Note = sequelize.define('Note', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [1, 200]
+    }
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  isPublic: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  },
+  subjectId: {
+    type: DataTypes.UUID,
+    allowNull: true,
+    references: {
+      model: 'subjects',
+      key: 'id'
+    }
+  }
+}, {
+  tableName: 'notes',
+  indexes: [
+    { fields: ['userId'] },
+    { fields: ['subjectId'] },
+    { fields: ['createdAt'] }
+  ]
+});
+
+module.exports = Note;
